b97a95eeeb36c1d3de2cb87e9f61b7b370fb8ae0
Allison Sass <allison.sass@gmail.com>
2015-10-08 17:25:27 -0500

    extra comments in js

--- app.js
+++ app.js
@@ -18,17 +18,19 @@ if ( i % 3 === 0) {
 	}
 }
 
-
+//if number is divisible by 5, show Buzz
 else if ( i % 5 === 0){
 
 	$("#body").append('Buzz'); 
 }
+//display integers if not divisible by 3 or 5
 
 else {
 
 	$("#body").append(i); 
 }
 
+//adding breaks
 $("#body").append("<br>");
 
 
